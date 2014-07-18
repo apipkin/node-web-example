7ce19a21fe3853a4e5d74b19eb465ec74a6a31db
Anthony Pipkin <a.pipkin@yahoo.com>
2014-07-18 16:07:30 -0400

    Add package and app

---
+++ app.js
@@ -0,0 +1,12 @@
+var Hapi = require('hapi'),
+    server = Hapi.createServer('0.0.0.0', +process.env.PORT || 3000);
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (req, res) {
+        res('Hello, world!');
+    }
+});
+
+server.start();
