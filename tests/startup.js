edc0c669d0d31aded68ad83371f92fef9cdff36b
Anthony Pipkin <a.pipkin@yahoo.com>
2014-07-24 13:02:26 -0400

    final commit

--- tests/startup.js
+++ tests/startup.js
@@ -1,5 +1,5 @@
 var Lab = require('lab'),
-    server = require('../app.js');
+    server = require('../lib/app.js');
 
 Lab.experiment('Users', function () {
 
@@ -34,6 +34,40 @@ Lab.experiment('Users', function () {
             Lab.expect(resp.statusCode).to.equal(200);
             Lab.expect(result).to.be.a('string');
             Lab.expect(result).to.equal('Hello, Node!');
+
+            done();
+        });
+    });
+
+    Lab.test('adding a param', function (done) {
+        var options = {
+            method: 'GET',
+            url: '/Tod/Copper'
+        };
+
+        server.inject(options, function (resp) {
+            var result = resp.result;
+
+            Lab.expect(resp.statusCode).to.equal(200);
+            Lab.expect(result).to.be.a('string');
+            Lab.expect(result).to.equal('Hello, Tod and Copper!');
+
+            done();
+        });
+    });
+
+    Lab.test('adding a param', function (done) {
+        var options = {
+            method: 'GET',
+            url: '/Tod/Copper/Bob'
+        };
+
+        server.inject(options, function (resp) {
+            var result = resp.result;
+
+            Lab.expect(resp.statusCode).to.equal(200);
+            Lab.expect(result).to.be.a('string');
+            Lab.expect(result).to.equal('Hello, Tod, Copper, and Bob!');
 
             done();
         });
